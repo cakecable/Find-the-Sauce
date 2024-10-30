9c0e54fd0e220b5a62bc14f6223e6c8ddad5781d
cake <164044366+cakecable@users.noreply.github.com>
2024-10-30 11:12:25 -0400

    Add files via upload

--- find-the-sauce/Interactions/InteractionArea/interaction_area.gd
+++ find-the-sauce/Interactions/InteractionArea/interaction_area.gd
@@ -6,3 +6,11 @@ class_name InteractionArea
 
 var interact: Callable = func():
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	InteractionManager.unregister_area(self)
